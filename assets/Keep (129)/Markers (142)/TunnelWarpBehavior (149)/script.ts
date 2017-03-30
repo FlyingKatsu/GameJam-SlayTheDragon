e1d2b619d24531d714e56313142ca1a7026b6bf2
Flying Katsu <Warped2713@users.noreply.github.com>
2017-03-30 02:26:58 -0400

    Collision Behavior for TunnelWarp according to #5

--- assets/Keep (129)/Markers (142)/TunnelWarpBehavior (149)/script.ts
+++ assets/Keep (129)/Markers (142)/TunnelWarpBehavior (149)/script.ts
@@ -1,8 +1,40 @@
 class TunnelWarpBehavior extends Sup.Behavior {
-  toPosition: Sup.Actor;
-
+  targetName:string = "HeroSpawner"; 
+  sensor: number = 0;
+  
+  private target: Sup.Actor;
+  private humanActors: Sup.Actor[] = [];
+  
+  awake() {
+    // Set target
+    this.target = Sup.getActor( this.targetName );
+    // Collect humanoid actors
+    this.humanActors.push( Sup.getActor("Player") );
+    for ( let heroActor of Sup.getActor("Heroes").getChildren() ) this.humanActors.push( heroActor );
+  }
+  
   update() {
     
+    // Check if colliding with any human actors, and if so, warp them to the target
+    for ( let humanActor of this.humanActors ) {
+      
+      // Using built-in collision
+      /*Sup.ArcadePhysics2D.collides( humanActor.arcadeBody2D, this.actor.arcadeBody2D );
+      if ( this.actor.arcadeBody2D.getTouches()[ Game.stringLocation[this.sensor] ] ) {
+        humanActor.arcadeBody2D.warpPosition(this.target.getPosition());
+      }*/
+      
+      // Using custom collision computation      
+      // If sensor location is top, only need to check bottom coordinates of humanBody with top coordinates of this body
+      if ( Collision2D.collides( 
+            this.actor.getBehavior(CollisionBehavior).getCollisionPoints(Game.stringLocation[this.sensor]), 
+            humanActor.getBehavior(CollisionBehavior).getCollisionPoints(Game.opposingLocation[this.sensor]) ) ) {
+          humanActor.arcadeBody2D.warpPosition(this.target.getPosition());
+      }
+      
+    }
+    
   }
+  
 }
 Sup.registerBehavior(TunnelWarpBehavior);
