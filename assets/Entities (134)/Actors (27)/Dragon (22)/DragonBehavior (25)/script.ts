36951b778226f549bdf626f8005d358ce72a1138
Flying Katsu <Warped2713@users.noreply.github.com>
2017-04-09 00:04:14 -0400

    food is destroyed when colliding with dragon

--- assets/Entities (134)/Actors (27)/Dragon (22)/DragonBehavior (25)/script.ts
+++ assets/Entities (134)/Actors (27)/Dragon (22)/DragonBehavior (25)/script.ts
@@ -6,7 +6,18 @@ class DragonBehavior extends Sup.Behavior {
   private hitboxes = [];
   private hitboxes2: Sup.ArcadePhysics2D.Body[] = [];
   
+  private head;
+  private belly;
+  private feet;
+  private tail;
+  
   awake() {
+    
+    this.head = this.actor.getChild("Hitbox").getChild("Head").getBehavior(HitBehavior);
+    this.belly = this.actor.getChild("Hitbox").getChild("Belly").getBehavior(HitBehavior);
+    this.feet = this.actor.getChild("Hitbox").getChild("Feet").getBehavior(HitBehavior);
+    this.tail = this.actor.getChild("Hitbox").getChild("Tail").getBehavior(HitBehavior);
+    
     this.hitboxes = this.actor.getChild("Hitbox").getChildren();
     for ( let hit of this.hitboxes ) {
       this.hitboxes2.push(hit.arcadeBody2D);
@@ -15,7 +26,7 @@ class DragonBehavior extends Sup.Behavior {
 
   update() {
     
-    if (this.counter > 3)  Game.endGame(true);
+    //if (this.counter > 3)  Game.endGame(true);
     
     // Update dialogue obj
     if ( this.dialogue.timer == 0 ) {
@@ -27,25 +38,51 @@ class DragonBehavior extends Sup.Behavior {
     
     let foodItems = Sup.getActor("Food").getChildren();
     for ( let foodBox of foodItems ) {
+      let destroyItem = false;
       for ( let hit of this.hitboxes2 ) {
         // Check intersection with food
         if ( Sup.ArcadePhysics2D.intersects( foodBox.arcadeBody2D, hit ) ) {
           this.counter++;
+          
           let text = "";
           for ( let i = 0; i < this.counter; i++ ) text += "<3";
           Game.data.dragon = text;
           Game.updateHUD();
           this.actor.getChild("Dialogue").textRenderer.setText(text);
           this.dialogue.timer = 60;
+          
+          destroyItem = true;
         }
       }
-      
+      if (destroyItem) foodBox.destroy();
     }
     // Collide with tilemap
     //Sup.ArcadePhysics2D.collides( this.actor.arcadeBody2D, Sup.getActor("Map").arcadeBody2D );
     
     // Check intersection with sword
     
+  }
+  
+  // Determine if dead; Only call when hit
+  checkDeathOnHit() {
+    if ( this.belly.defense == 0 || this.head.defense == 0 ) {
+      // TODO: Play dead
+      this.dialogue.text = "Slayed!";
+      this.dialogue.timer = 60;
+      Game.data.dragon = "Slayed!";
+      Game.updateHUD();
+    } else {
+      let text = "";
+      for ( let i = 0; i < this.counter; i++ ) text += "<3 ";
+      if (this.counter < 0) {
+        text = "";
+        for ( let i = this.counter; i <= 0; i++ ) text += ">:(";
+      }
+      this.dialogue.text = text;
+      this.dialogue.timer = 60;
+      Game.data.dragon = text;
+      Game.updateHUD();
+    }
   }
 }
 Sup.registerBehavior(DragonBehavior);
